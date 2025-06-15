d757c5ff2425e4dc2658f083f4e465004639a109
code-shams <code.meshams@gmail.com>
2025-06-15 19:20:33 +0600

    Updated Feature Cards

--- src/pages/EventPages/Upcoming.jsx
+++ src/pages/EventPages/Upcoming.jsx
@@ -1,8 +1,14 @@
 import { useLoaderData } from "react-router";
+import { format } from "date-fns";
 
 const Upcoming = () => {
     const eventData = useLoaderData();
     console.log(eventData);
+    // for (const event of eventData) {
+    //     const isoDate = new Date(event.eventDate);
+    //     const formatted = format(isoDate, "dd/MM/yyyy");
+    //     console.log(formatted);
+    // }
     return <div>Upcoming</div>;
 };
 
--- src/pages/Home/Features.jsx
+++ src/pages/Home/Features.jsx
@@ -1,4 +1,4 @@
-import { UserPlus, UserRoundPen, FilePlus } from "lucide-react";
+import { UserPlus, FilePlus, CalendarRange } from "lucide-react";
 import React from "react";
 import { Link } from "react-router";
 
@@ -11,23 +11,24 @@ const Features = () => {
             {/* Cards */}
             <section className="flex flex-col md:flex-row gap-1 md:gap-5 md:justify-between pt-3  md:pt-6">
                 {/* CARD1 */}
-                <div className="flex flex-col gap-1 md:gap-3 bg-secondary/30 drop-shadow-sm border-2 border-secondary/50 p-4 text-center rounded-2xl scale-95 hover:scale-100 transition-all duration-500">
+                <div className="flex flex-col gap-3 bg-secondary/30 drop-shadow-sm border-2 border-secondary/50 p-4 text-center rounded-2xl scale-95 hover:scale-100 transition-all duration-500">
                     <h1 className="sec-font text-lg md:text-xl font-bold text-accent drop-shadow-lg">
-                        Create Event
+                        Explore Events
                     </h1>
                     <p className="text-sm md:text-base pb-2 md:pb-0">
-                        Turn your vision into impact. Our platform simplifies
-                        organizing and promoting social development events,
-                        helping you rally participants and create positive
-                        change effortlessly.
+                        Discover a variety of upcoming events focused on social
+                        development. Find opportunities to participate, learn,
+                        and contribute to causes that matter to you.
                     </p>
-                    <Link className="mt-auto btn btn-primary btn-xs md:btn-sm hover:bg-accent hover:border-none transition-all duration-300 shadow-sm w-max mx-auto rounded-full">
-                        <FilePlus className="size-4"/>
-                        Start an Event
+                    <Link
+                        to="event/upcoming"
+                        className="mt-auto btn btn-primary btn-xs md:btn-sm hover:bg-accent hover:border-none transition-all duration-300 shadow-sm w-max mx-auto rounded-full"
+                    >
+                        <CalendarRange className="size-4" />
+                        Our Events
                     </Link>
                 </div>
                 {/* card2 */}
-
                 <div className="flex flex-col gap-3 bg-secondary/30 drop-shadow-sm border-2 border-secondary/50 p-4 text-center rounded-2xl scale-95 hover:scale-100 transition-all duration-500">
                     <h1 className="sec-font text-lg md:text-xl font-bold text-accent drop-shadow-lg">
                         Join the Community
@@ -38,26 +39,31 @@ const Features = () => {
                         skills, and collaborate for shared inspiration and
                         collective impact.
                     </p>
-                    <Link className="mt-auto btn btn-primary btn-xs md:btn-sm hover:bg-accent hover:border-none transition-all duration-300 shadow-sm w-max mx-auto rounded-full">
+                    <Link
+                        to="/sign-in"
+                        className="mt-auto btn btn-primary btn-xs md:btn-sm hover:bg-accent hover:border-none transition-all duration-300 shadow-sm w-max mx-auto rounded-full"
+                    >
                         <UserPlus className="size-4" />
-                        Explore Community
+                        Join Community
                     </Link>
                 </div>
-
                 {/* card3 */}
-                <div className="flex flex-col gap-3 bg-secondary/30 drop-shadow-sm border-2 border-secondary/50 p-4 text-center rounded-2xl scale-95 hover:scale-100 transition-all duration-500">
+                <div className="flex flex-col gap-1 md:gap-3 bg-secondary/30 drop-shadow-sm border-2 border-secondary/50 p-4 text-center rounded-2xl scale-95 hover:scale-100 transition-all duration-500">
                     <h1 className="sec-font text-lg md:text-xl font-bold text-accent drop-shadow-lg">
-                        Manage Events
+                        Create Event
                     </h1>
                     <p className="text-sm md:text-base pb-2 md:pb-0">
-                        Effortlessly oversee your initiatives. Our dashboard
-                        offers tools to track registrations, send updates,
-                        communicate with volunteers, and monitor event progress,
-                        so you can focus on impact.
+                        Turn your vision into impact. Our platform simplifies
+                        organizing and promoting social development events,
+                        helping you rally participants and create positive
+                        change effortlessly.
                     </p>
-                    <Link className="mt-auto btn btn-primary btn-xs md:btn-sm hover:bg-accent hover:border-none transition-all duration-300 shadow-sm w-max mx-auto rounded-full">
-                        <UserRoundPen className="size-4" />
-                        My Events
+                    <Link
+                        to="event/create"
+                        className="mt-auto btn btn-primary btn-xs md:btn-sm hover:bg-accent hover:border-none transition-all duration-300 shadow-sm w-max mx-auto rounded-full"
+                    >
+                        <FilePlus className="size-4" />
+                        Start an Event
                     </Link>
                 </div>
             </section>
