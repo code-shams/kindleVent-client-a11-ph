97e48f26de77466443d85db76888e491fee55dc3
code-shams <code.meshams@gmail.com>
2025-06-17 02:01:18 +0600

    Added loading state to join events page

--- src/pages/EventPages/JoinEvent.jsx
+++ src/pages/EventPages/JoinEvent.jsx
@@ -4,11 +4,13 @@ import { AuthContext } from "../../contexts/AuthProvider";
 import { useNavigate } from "react-router";
 import Swal from "sweetalert2";
 import EventCard from "../../components/EventCard";
+import Loader from "../../components/Loader/Loader";
 
 const JoinEvent = () => {
     const navigate = useNavigate();
     const { axiosSecure } = useAxios();
-    const { user, logoutUser, setLoading } = use(AuthContext);
+    const { user, logoutUser } = use(AuthContext);
+    const [loading, setLoading] = useState(true);
     const [joinedEvents, setJoinedEvents] = useState([]);
     useEffect(() => {
         const getData = async () => {
@@ -17,10 +19,11 @@ const JoinEvent = () => {
                     `/events/joined?email=${user?.email}`
                 );
                 setJoinedEvents(data.data);
+                setLoading(false);
             } catch (err) {
+                setLoading(false);
                 logoutUser();
                 navigate("/sign-in");
-                setLoading(false);
                 Swal.fire({
                     title: `${err.status} Error!`,
                     text: `${err.response.data.message}`,
@@ -29,35 +32,39 @@ const JoinEvent = () => {
                 });
             }
         };
-        if (user?.email) {
-            getData();
-        }
+        getData();
     }, [user, axiosSecure]);
 
     return (
-        <div className="pri-font bg-secondary/20 border-2 border-secondary/10 rounded-xl shadow-lg p-0 md:p-4 drop-shadow-lg">
-            {joinedEvents.length === 0 ? (
-                <h1 className="text-lg md:text-3xl font-medium text-center px-2 md:px-10 w-max mx-auto drop-shadow-lg sec-font">
-                    You haven't joined any events yet!
-                </h1>
+        <>
+            {loading ? (
+                <Loader></Loader>
             ) : (
-                <>
-                    <h1 className="text-lg md:text-3xl font-medium text-center px-2 md:px-10 w-max mx-auto drop-shadow-lg sec-font">
-                        Joined Events
-                    </h1>
-                    {/* All Event Cards Container */}
-                    <div className="contain grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-1 md:gap-3 pt-1">
-                        {/* Individual Card Container */}
-                        {joinedEvents.map((eventInfo) => (
-                            <EventCard
-                                key={eventInfo._id}
-                                eventInfo={eventInfo}
-                            ></EventCard>
-                        ))}
-                    </div>
-                </>
+                <div className="pri-font bg-secondary/20 border-2 border-secondary/10 rounded-xl shadow-lg p-0 md:p-4 drop-shadow-lg">
+                    {joinedEvents.length === 0 ? (
+                        <h1 className="text-lg md:text-3xl font-medium text-center px-2 md:px-10 w-max mx-auto drop-shadow-lg sec-font">
+                            You haven't joined any events yet!
+                        </h1>
+                    ) : (
+                        <>
+                            <h1 className="text-lg md:text-3xl font-medium text-center px-2 md:px-10 w-max mx-auto drop-shadow-lg sec-font">
+                                Joined Events
+                            </h1>
+                            {/* All Event Cards Container */}
+                            <div className="contain grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-1 md:gap-3 pt-1">
+                                {/* Individual Card Container */}
+                                {joinedEvents.map((eventInfo) => (
+                                    <EventCard
+                                        key={eventInfo._id}
+                                        eventInfo={eventInfo}
+                                    ></EventCard>
+                                ))}
+                            </div>
+                        </>
+                    )}
+                </div>
             )}
-        </div>
+        </>
     );
 };
 
